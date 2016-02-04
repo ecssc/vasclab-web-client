fe30b95c1466d72a99ccb04dc4c52f4ab16d4ca1
Edward Coleridge Smith <e.coleridgesmith@edcs.me>
2016-02-04 08:10:53 +0000

    Refactored default view

---
+++ app/containers/HomePage.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux'
+import DefaultLayout from '../components/DefaultLayout'
+
+const mapStateToProps = (state) => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    }
+}
+
+const HomePage = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(DefaultLayout)
+
+export default HomePage
