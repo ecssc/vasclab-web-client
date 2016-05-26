52dbe4ab1db661a14eddcbe13c0aa07200fca486
Edward Coleridge Smith <e.coleridgesmith@edcs.me>
2016-05-26 08:38:44 +0100

    Updated action types

--- app/state/action-types.js
+++ app/state/action-types.js
@@ -1,6 +1,8 @@
-export const SHOW_MAIN_NAV = 'SHOW_MAIN_NAV'
-export const HIDE_MAIN_NAV = 'HIDE_MAIN_NAV'
-export const TOGGLE_MAIN_NAV = 'TOGGLE_MAIN_NAV'
+export const SHOW_MAIN_NAV     = 'SHOW_MAIN_NAV'
+export const HIDE_MAIN_NAV     = 'HIDE_MAIN_NAV'
+export const TOGGLE_MAIN_NAV   = 'TOGGLE_MAIN_NAV'
 
-export const USER_LOGIN = 'USER_LOGIN'
-export const USER_LOGOUT = 'USER_LOGOUT'
+export const USER_AUTH_ATTEMPT = 'USER_AUTH_ATTEMPT'
+export const USER_AUTH_FAIL    = 'USER_AUTH_FAIL'
+export const USER_AUTH_SUCCESS = 'USER_AUTH_SUCCESS'
+export const USER_AUTH_LOGOUT  = 'USER_AUTH_LOGOUT'
