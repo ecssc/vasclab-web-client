972cb620c6decf61d102469943d67ee407ac6114
Edward Coleridge Smith <e.coleridgesmith@edcs.me>
2016-07-05 09:19:57 +0100

    Make it possible to refresh an access token

--- app/state/sagas/user-auth-check.js
+++ app/state/sagas/user-auth-check.js
@@ -1,7 +1,7 @@
 import { takeEvery } from 'redux-saga';
 import { put } from 'redux-saga/effects';
 import * as types from '../action-types';
-import { user } from '../../api/client';
+import { user, auth } from '../../api/client';
 
 /**
  * Watches for user auth check state change.
@@ -11,7 +11,7 @@ export default function* () {
 }
 
 /**
- * Attempts to validate a user's credentials via the api.
+ * Attempts to authenticate a user via the api.
  *
  * @param {*} action
  */
@@ -19,16 +19,53 @@ const checkUserStatus = function* (action) {
     yield put({ type: types.SHOW_PROGRESS_BAR });
 
     try {
+        yield checkForAuthenticatedUser(action);
+    } catch (error) {
+        yield put({ type: types.USER_AUTH_LOGOUT });
+    } finally {
+        yield put({ type: types.HIDE_PROGRESS_BAR });
+    }
+}
+
+/**
+ * Checks to see whether or not the user has a valid access token.
+ *
+ * @param {*} action
+ * @return {*}
+ */
+const checkForAuthenticatedUser = function* (action) {
+    try {
         let me = yield user.me().then((response) => {
             return response.body.data;
         }).catch((error) => {
             throw error;
         });
 
+        console.info('User '+me.id+' was authenticated.');
+
         yield put({ type: types.USER_AUTH_SUCCESS, user: me });
     } catch (error) {
-        yield put({ type: types.USER_AUTH_LOGOUT });
-    } finally {
-        yield put({ type: types.HIDE_PROGRESS_BAR });
+        console.warn('No access token available - attempting to use refresh token.');
+        yield refreshAccessToken(action);
+    }
+}
+
+/**
+ * Attempts to refresh the current user's access token.
+ *
+ * @param {*} action
+ */
+const refreshAccessToken = function* (action) {
+    try {
+        yield auth.refreshToken().then((response) => {
+            return response.body;
+        }).catch((error) => {
+            throw error;
+        });
+
+        yield checkForAuthenticatedUser(action);
+    } catch (error) {
+        console.warn('No refresh token available - logging user out.');
+        throw error;
     }
 }
