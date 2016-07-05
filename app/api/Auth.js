4ae071d0ef4c61d8f3bec73e88911e0a8895f0ce
Edward Coleridge Smith <e.coleridgesmith@edcs.me>
2016-07-05 09:20:34 +0100

    Add refresh token method to auth model

--- app/api/Auth.js
+++ app/api/Auth.js
@@ -15,6 +15,20 @@ class Auth extends Api {
             client_id: config.api.client_id,
             username: username,
             password: password
+        });
+    }
+
+    /**
+     * A promise request for refreshing an access token.
+     *
+     * @param {string} username
+     * @param {string} password
+     * @return {Promise}
+     */
+    refreshToken() {
+        return this.post('oauth/access_token', {
+            grant_type: 'refresh_token',
+            client_id: config.api.client_id
         })
     }
 }
