d4cc2633b38cb96f2dc59671b3eb70f5e25d38f1
Edward Coleridge Smith <e.coleridgesmith@edcs.me>
2016-07-28 15:36:56 +0100

    Add logout saga

--- app/state/sagas/user-auth-logout.js
+++ app/state/sagas/user-auth-logout.js
@@ -1,19 +1,30 @@
 import { takeEvery } from 'redux-saga';
+import { put } from 'redux-saga/effects';
 import { browserHistory } from 'react-router';
-import { USER_AUTH_LOGOUT } from '../action-types';
+import { USER_AUTH_LOGOUT, SHOW_SNACKBAR } from '../action-types';
+import { auth } from '../../api/client';
 
 /**
  * Watches for user auth success state change.
  */
 export default function* () {
-    yield* takeEvery(USER_AUTH_LOGOUT, userWasLoggedOut);
+    yield* takeEvery(USER_AUTH_LOGOUT, logUserout);
 }
 
 /**
- * Called when a user was succesfully authenticated.
+ * Called when a user was logged out.
  *
  * @param {*} action
  */
-const userWasLoggedOut = function* (action) {
-    yield browserHistory.push('/login');
+const logUserout = function* (action) {
+    try {
+        yield auth.revokeToken();
+        yield browserHistory.push('/login');
+    } catch (error) {
+        yield put({
+            type: SHOW_SNACKBAR,
+            message: 'We couldn\'t sign you out - please try again',
+            action: 'Ok'
+        });
+    }
 }
