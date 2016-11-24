92cabc946efa9eb8f0878eda16ef5aa39bfbafa9
Edward Coleridge Smith <e.coleridgesmith@edcs.me>
2016-11-24 13:07:34 +0000

    Update CS

---
+++ src/pages/ReportPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import DefaultLayout from '../layouts/Default';
+import ReportCard from '../containers/ReportCard';
+
+const ReportPage = () => (
+    <DefaultLayout title="VascLab">
+        <ReportCard />
+    </DefaultLayout>
+);
+
+export default ReportPage;
