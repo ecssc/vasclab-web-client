f80d7e3c4d0b298453e8e1c6866bb5e21dfe2af1
Edward Coleridge Smith <e.coleridgesmith@edcs.me>
2016-11-30 16:09:00 +0000

    Setup Jest testing

--- src/functions/__tests__/helpers.test.js
+++ src/functions/__tests__/helpers.test.js
@@ -7,9 +7,15 @@ test('Number is left padded', () => {
 });
 
 test('Number is left padded with specifed value', () => {
-    const padded = leftPad(1, 5, 1);
+    const padded = leftPad(1, 5, 2);
 
-    expect(padded).toEqual('11111');
+    expect(padded).toEqual('22221');
+});
+
+test('Number is not left padded if same length as padding', () => {
+    const padded = leftPad(11, 2);
+
+    expect(padded).toEqual('11');
 });
 
 test('Number is right padded', () => {
@@ -19,7 +25,13 @@ test('Number is right padded', () => {
 });
 
 test('Number is right padded with specifed value', () => {
-    const padded = leftPad(1, 5, 1);
+    const padded = rightPad(1, 5, 2);
 
-    expect(padded).toEqual('11111');
+    expect(padded).toEqual('12222');
+});
+
+test('Number is not right padded if same length as padding', () => {
+    const padded = rightPad(11, 2);
+
+    expect(padded).toEqual('11');
 });
